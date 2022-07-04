9d0f98a442066fe10475a30458672b59c4e17069
Farouk Ech-charef <fech-cha@e1r5p12.1337.ma>
2022-07-04 01:31:42 +0100

    learn cpp

---
+++ 1_I&o.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+
+	//read 2 input in the same line with a whitespace
+	// string	str;
+	// int		n;
+
+	// std::cin >> str >> n;
+	// std::cout << "string is : " << str << "\nInteger is : " << n << "\n";
+	//Read 3 numbers from stdin and print their sum to stdout
+	 int n1, n2, n3;
+    
+    std::cin >> n1 >> n2 >> n3 ;
+    std::cout << n1+n2+n3;
+    return 0;
+
+    return 0;
+}
